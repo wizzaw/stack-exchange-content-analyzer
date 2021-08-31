348dd7b7b22ed37adfeec5aa7173899b33b583f7
Gencer Nayki <gencer.nayki@mendix.com>
2021-08-31 05:59:01 +0200

    Rest endpoint and redis cache store implementation

---
+++ src/main/java/com/nayki/models/AnalysisState.java
@@ -0,0 +1,8 @@
+package com.nayki.models;
+
+public enum AnalysisState {
+    ANALYZING,
+    FINISHED,
+    DELETING,
+    FAILED
+}
---
+++ src/main/java/com/nayki/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package com.nayki.utils;
+
+public class StringUtils {
+
+    public static boolean isBlank(final String string) {
+        return string == null || string.isBlank();
+    }
+}
